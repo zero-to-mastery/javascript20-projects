53a04611f2b343b676cf8bff060000c333a3d4a7
TaraByte79 <swingholm79@gmail.com>
2021-07-25 11:57:44 -0500

    Add TaraByte79 and project to list

---
+++ script.js
@@ -0,0 +1,28 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// Prompt to select media stram, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // Catch Error Here
+    console.log("whoops, error here", error);
+  }
+}
+
+button.addEventListener("click", async () => {
+  // Disable Button
+  button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  // Reset Button
+  button.disabled = false;
+});
+
+// On Load
+selectMediaStream();
