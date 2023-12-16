90671147eec8ae25d26b25fddad3ae7077371236
Lambru Adrian Georgian <120751654+lambrugeorge@users.noreply.github.com>
2023-12-17 00:11:31 +0200

    Joke Teller

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
-const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
+const audioElement = document.getElementById('audio');
 
-async function selectMediaStream() {
-    try {
-        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
-        videoElement.srcObject = mediaStream;
-        videoElement.onloadedmetadata = () => {
-            videoElement.play();
-        }
-    } catch (error) {
-        console.log('whoops, error here:', error);
-    }
-
+// Disable/Enable Button
+function toggleButton() {
+  button.disabled = !button.disabled;
 }
 
-button.addEventListener('click', async () => {
-    button.disabled = true;
-    await videoElement.requestPictureInPicture()
-        button.disabled = false;
-});
+// VoiceRSS Speech Function
+function tellMe(joke) {
+  const jokeString = joke.trim().replace(/ /g, '%20');
+  // VoiceRSS Speech Parameters
+  VoiceRSS.speech({
+    // Normally, don't write out API Keys like this, but an exception made here because it's free.
+    key: 'b73d990a106144c4aee6af06ef7537eb',
+    src: jokeString,
+    hl: 'en-us',
+    r: 0,
+    c: 'mp3',
+    f: '44khz_16bit_stereo',
+    ssml: false,
+  });
+}
 
-selectMediaStream();
+// Get jokes from Joke API
+async function getJokes() {
+  let joke = '';
+  const apiUrl = 'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,racist,sexist';
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    // Assign One or Two Part Joke
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    // Passing Joke to VoiceRSS API
+    tellMe(joke);
+    // Disable Button
+    toggleButton();
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+// Event Listeners
+button.addEventListener('click', getJokes);
+audioElement.addEventListener('ended', toggleButton);
